e8f695529e547e9907859eb41dfc6fd06976235d
Rajrup Das <166900923+coder-rajrup@users.noreply.github.com>
2025-05-14 20:09:42 +0530

    Update StudentGradeCalculatorSimple.java

--- StudentGradeCalculatorSimple.java
+++ StudentGradeCalculatorSimple.java
@@ -3,24 +3,15 @@ import java.util.Scanner;
 public class StudentGradeCalculatorSimple {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input number of subjects
         System.out.print("Enter number of subjects: ");
         int n = sc.nextInt();
-
         int total = 0;
-
-        // Input marks and calculate total
         for (int i = 1; i <= n; i++) {
             System.out.print("Enter marks for subject " + i + ": ");
             int marks = sc.nextInt();
             total += marks;
         }
-
-        // Calculate average
         float average = (float) total / n;
-
-        
         String grade;
         if (average >= 90) grade = "A+";
         else if (average >= 80) grade = "A";
@@ -28,8 +19,6 @@ public class StudentGradeCalculatorSimple {
         else if (average >= 60) grade = "C";
         else if (average >= 50) grade = "D";
         else grade = "F";
-
-        // Display result
         System.out.println("\nTotal Marks: " + total);
         System.out.println("Average Percentage: " + average + "%");
         System.out.println("Grade: " + grade);
